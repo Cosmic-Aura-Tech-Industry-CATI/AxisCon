bf91e2091cfd754f25c463f714af82f2f05b4fc6
Shikhar Dixit <dixitshikhar004@gmail.com>
2026-02-19 13:41:56 +0530

    Fix case-sensitive import paths in Committee.jsx and Theme.jsx

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import "./committee.css";
+import "./Committee.css";
 
 const organizingCommittee = [
   {
--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import "./theme.css";
+import "./Theme.css";
 
 const themes = [
   {
